3e15bcd1ebe531b15b7943acf8005fa927166d02
Carlos Gumucio <cgumuciolabbe@gmail.com>
2022-01-15 23:41:49 -0300

    last update

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, ImageBackground, Pressable } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
+  const navigation = useNavigation();
   const background = {
     uri: 'https://cdn.vox-cdn.com/thumbor/rQdP_2gdCKFQU7nPcKK8cuX2BDI=/0x0:1898x1273/1200x800/filters:focal(833x627:1135x929)/cdn.vox-cdn.com/uploads/chorus_image/image/64128012/airbnb_luxe_announcement.0.jpg',
   };
@@ -28,7 +30,7 @@ const HomeScreen = ({ navigation }) => {
 
         <Pressable
           style={styles.button}
-          onPress={() => console.warn('Explore Btn clicked')}
+          onPress={() => navigation.navigate('Destination Search')}
         >
           <Text style={styles.buttonText}>Explore Nearby Places</Text>
         </Pressable>
